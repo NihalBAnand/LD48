1fca34f63cae9942bd58c9f5c1950c21577f9a0d
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2021-04-26 00:48:02 -0500

    SWOORD

--- LD48/Assets/Scripts/PlayerController.cs
+++ LD48/Assets/Scripts/PlayerController.cs
@@ -34,6 +34,9 @@ public class PlayerController : MonoBehaviour
 
     public GameObject opcont;
 
+    public GameObject sword;
+    public bool attacking;
+
     void Start()
     {
         health = 7;
@@ -65,6 +68,11 @@ public class PlayerController : MonoBehaviour
             {
                 GameObject.Find("UIController").GetComponent<UIController>().CreateTextbox(cultText);
 
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                attack();
             }
 
             if (health > 0)
@@ -141,5 +149,19 @@ public class PlayerController : MonoBehaviour
         GameObject.Find("HealthBar").GetComponent<Image>().color = new Color32(255, 105, 105, 255);
         yield return new WaitForSecondsRealtime(.2f);
         GameObject.Find("HealthBar").GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+    }
+
+    void attack()
+    {
+        if (GameObject.Find("sword(Clone)") == null)
+        {
+            GameObject newSword = Instantiate(sword);
+            
+            newSword.transform.parent = gameObject.transform;
+            
+            newSword.GetComponent<Animator>().Play("sword");
+            GameObject.Destroy(newSword, newSword.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+            
+        }
     }
 }
---
+++ LD48/Assets/Scripts/Sword.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        switch (transform.parent.GetComponent<PlayerController>().facing)
+        {
+            case "Down":
+                direction = new Vector2(0, -1);
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = 5;
+                break;
+            case "Up":
+                direction = new Vector2(0, 1);
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = 3;
+                break;
+            case "Left":
+                direction = new Vector2(-1, -.25f);
+                gameObject.transform.localScale = new Vector2(-1, 1);
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = 3;
+                break;
+            case "Right":
+                direction = new Vector2(1, 0);
+                gameObject.GetComponent<SpriteRenderer>().sortingOrder = 3;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (transform.parent.GetComponent<PlayerController>().facing)
+        {
+            case "Down":
+                gameObject.transform.position = direction + (Vector2)transform.parent.position;
+                
+                break;
+            case "Up":
+                gameObject.transform.position = direction + (Vector2)transform.parent.position;
+                
+                break;
+            case "Left":
+                gameObject.transform.position = direction + (Vector2)transform.parent.position;
+                
+                break;
+            case "Right":
+                gameObject.transform.position = direction + (Vector2)transform.parent.position;
+                
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //do the combat stoof here, GL
+    }
+}
